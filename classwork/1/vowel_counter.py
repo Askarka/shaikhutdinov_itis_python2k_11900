a66960a630d84acf939be3f5cf6cf23b61ce8b44
Askar <askarkaka@icloud.com>
2020-11-16 23:59:01 +0300

    remade first task

--- classwork/1/vowel_counter.py
+++ classwork/1/vowel_counter.py
@@ -3,6 +3,6 @@ array_of_vowels = ['a', 'e', 'y', 'u', 'i', 'o']
 num_of_vowels = 0
 for i in string_to_scan:
     for j in array_of_vowels:
-        if i == j:
+        if i == j or i == j.upper():
             num_of_vowels+=1
 print('Number of vowels is ' + str(num_of_vowels) + '.')
